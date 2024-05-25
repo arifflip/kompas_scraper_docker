4b5464c7e28307754fd4f39fb2a1c32fd6739f2f
arifflip <newarh14@gmail.com>
2024-05-25 19:22:24 +0700

    modify - remove sql syntax to crerate raw_scraping_result table

--- dags/create_tabel.sql
+++ dags/create_tabel.sql
@@ -1,17 +1,12 @@
--- create raw scraping result
-CREATE TABLE IF NOT EXISTS raw_scraping_result (
-    tanggal DATE NOT NULL,
-    response VARCHAR NOT NULL);
-
 -- create clean scraping result
-CREATE TABLE IF NOT EXISTS clean_scraping_result (
-    id SERIAL,
-    response VARCHAR NOT NULL,
+CREATE TABLE IF NOT EXISTS kompas_scraping_result (
+    tanggal date default current_timestamp,
+    scrape_time VARCHAR NOT NULL,
     story_source VARCHAR NOT NULL,
     story_news_tag VARCHAR NOT NULL,
-    story_release_date DATE NOT NULL,
+    story_release_date VARCHAR NOT NULL,    
     story_title VARCHAR NOT NULL,
-    story_url DATE NOT NULL,
+    story_url VARCHAR NOT NULL,
     most_common_words VARCHAR NOT NULL);
 
 
